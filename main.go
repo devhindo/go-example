3b3d658ff23eed8374ddf2b6a361e9b14c99c93e
hindawi <ahmedhindawi2001@gmail.com>
2023-09-24 22:50:03 +0300

    idkv2

--- main.go
+++ main.go
@@ -7,25 +7,8 @@ import (
 )
 
 func main() {
-    err := OpenDefault("F:\\task.txt")
-    if err != nil {
-        fmt.Println(err)
-    }
+
+    fmt.Println("idk")
+
 }
 
-func OpenDefault(fileOrURL string) error {
-	var cmd string
-	var args []string
-
-	switch runtime.GOOS {
-	case "windows":
-        cmd = "rundll32"
-        args = []string{"url.dll,FileProtocolHandler"}
-	case "darwin":
-		cmd = "open"
-	default: // "linux", "freebsd", "openbsd", "netbsd"
-		cmd = "xdg-open"
-	}
-	args = append(args, fileOrURL)
-	return exec.Command(cmd, args...).Start()
-}
